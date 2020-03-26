7864d1aae0a4b2b72dde5f03ce2cd32ee452783f
Camila Moura <camilapaleo@gmail.com>
2020-03-26 15:46:32 +0100

    create login page and global css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import './global.css';
+
+import Login from './pages/Login';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Login/>
   );
 }
 
 export default App;
+
