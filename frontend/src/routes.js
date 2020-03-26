62cf125cc4c80ce864ec301c34bc6de548fba2c1
Camila Moura <camilapaleo@gmail.com>
2020-03-26 22:03:50 +0100

    create profile page

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,14 +3,15 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Login}/>
-                <Route path="/register" component={Register}/>
-
+                <Route path="/" exact component={Login} />
+                <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
             </Switch>
         </BrowserRouter>
     );
