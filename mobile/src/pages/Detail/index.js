9cbc01723e3fc9d55156d1a3510a60d184df1e57
Camila Moura <camilapaleo@gmail.com>
2020-03-27 15:55:28 +0100

    create mobile app

---
+++ mobile/src/pages/Detail/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import logoImg from '../../assets/logo.png';
+
+import styles from './styles';
+
+export default function Detail() {
+    return (
+        <View style={styles.container}>
+           
+        </View>
+    );
+}
